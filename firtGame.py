35d76aa5026f858d475f324c31ed3d934fb2a940
JelsinPalomino <jstpalomino@hotmail.com>
2023-04-08 18:45:25 -0500

    Jump and gravity

--- firtGame.py
+++ firtGame.py
@@ -10,7 +10,7 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render('My game', False, 'Black')
+score_surf = test_font.render('My game', False, (64,64,64))
 score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -18,32 +18,38 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        """ if event.type == pygame.MOUSEMOTION:
-            if player_rect.collidepoint(event.pos): print('collision') """
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos): 
+                player_gravity = -20
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20 
+
+        if event.type == pygame.KEYUP:
+            print('key up')
     
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface,(0,300))
     #pygame.draw.rect(screen, 'Pink', score_rect)
-    pygame.draw.rect(screen, 'Pink', score_rect,0,10)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect,0,10)
     screen.blit(score_surf, score_rect)
 
     snail_rect.x -=4
     if snail_rect.right <= 0: snail_rect.left=800
     screen.blit(snail_surf, snail_rect)
+
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surf, player_rect)
-
-    """ if player_rect.colliderect(snail_rect):
-        print('collision') """
-
-    """ mouse_pos = pygame.mouse.get_pos()
-    if player_rect.collidepoint(mouse_pos):
-        print(pygame.mouse.get_pressed()) """
 
     pygame.display.update()
     clock.tick(60)
